bc92c34a23a384f7d0f0b3324e00ec292ce41f34
Ankit Bajpai <ankitbajpaib070@gmail.com>
2017-02-25 21:42:12 +0530

    Implementing custom logging. Logging using Trace.

---
+++ LoggingAndTracing/LoggingAndTracing/Logger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace EdgeVervePractice
+{
+    public static class Logger
+    {
+        public static void Log(Exception exp, string logType, string filePath = null)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("**************************************************************************************").Append(Environment.NewLine);
+            sb.Append("Logging Started at:").Append(Environment.NewLine);
+            sb.Append(DateTime.Now.ToString()).Append(Environment.NewLine);
+            do
+            {
+                sb.Append("Exception Type:").Append(Environment.NewLine);
+                sb.Append(exp.GetType().Name).Append(Environment.NewLine).Append(Environment.NewLine);
+
+                sb.Append("Message:").Append(Environment.NewLine);
+                sb.Append(exp.Message).Append(Environment.NewLine).Append(Environment.NewLine);
+
+                sb.Append("Stack Trace:").Append(Environment.NewLine);
+                sb.Append(exp.StackTrace).Append(Environment.NewLine).Append(Environment.NewLine);
+
+                exp = exp.InnerException;
+            } while (exp != null && exp.InnerException != null);
+
+            if (logType.Equals("file"))
+            {
+                if (filePath != null)
+                    LogToFile(sb.ToString(), filePath);
+            }
+            else if (logType.Equals("Trace"))
+                TraceError(sb.ToString());
+        }
+
+        public static void LogToFile(string message, string path)
+        {
+            using (FileStream fs = new FileStream(path + "\\Log.txt", FileMode.Append, FileAccess.Write))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(Environment.NewLine + message);
+                sw.Close();
+            }
+
+        }
+
+        public static void TraceError(string message)
+        {
+            Trace.WriteLine(message, "Trace Error");
+        }
+    }
+}
---
+++ LoggingAndTracing/LoggingAndTracing/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using EdgeVervePractice;
+
+namespace LoggingAndTracing
+{
+    class Program
+    {
+        private static void CustomLoggingDemo()
+        {
+            string logPath = "D:\\Logs";
+
+            if (!Directory.Exists(logPath))
+                Directory.CreateDirectory(logPath);
+
+            do
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    int c = a / b;
+                    Console.WriteLine(c);
+                }
+                catch (DivideByZeroException e)
+                {
+                    //logging using custom logger
+                    Logger.Log(e, "file", logPath);
+
+                    //logging via tracing, setting done in app.config
+                    Logger.Log(e, "Trace", logPath);
+                }
+
+                finally
+                {
+                    if (Directory.Exists(logPath) && File.Exists(logPath + "\\Log.txt"))
+                    {
+                        using (FileStream fs = new FileStream(logPath + "\\Log.txt", FileMode.Open, FileAccess.Read))
+                        {
+                            StreamReader sr = new StreamReader(fs);
+                            string line = null;
+
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                Console.WriteLine(line);
+                            }
+
+                            sr.Close();
+                        }
+                    }
+                }
+
+            } while (true);
+        }
+
+        private static void TracingDemo()
+        {
+            string sProdName = "Widget";
+            int iUnitQty = 100;
+            double dUnitCost = 1.03;
+
+            //Debug.WriteLine("Debug Information-Product Starting ");
+            //Debug.Indent();
+            //Debug.WriteLine("The product name is " + sProdName);
+            //Debug.WriteLine("The available units on hand are" + iUnitQty.ToString());
+            //Debug.WriteLine("The per unit cost is " + dUnitCost.ToString());
+
+            Debug.WriteLine("The product name is " + sProdName, "Field");
+            Debug.WriteLine("The units on hand are " + iUnitQty, "Field");
+            Debug.WriteLine("The per unit cost is " + dUnitCost.ToString(), "Field");
+            Debug.WriteLine("Total Cost is  " + (iUnitQty * dUnitCost), "Calc");
+
+            Debug.WriteLineIf(iUnitQty > 50, "Units of hand greater than 50.","Conditional");
+
+            Debug.Assert(dUnitCost > 1, "The per unit cost is very small");
+
+            TextWriterTraceListener tr1 = new TextWriterTraceListener(System.Console.Out);
+            Debug.Listeners.Add(tr1);
+
+            TextWriterTraceListener tr2 = new TextWriterTraceListener(System.IO.File.CreateText("Output.txt"));
+            Debug.Listeners.Add(tr2);
+
+        }
+
+        static void Main(string[] args)
+        {
+            CustomLoggingDemo();
+
+            //TracingDemo();
+
+            Console.ReadKey();
+        }
+    }
+}
